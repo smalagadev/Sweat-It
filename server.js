b01a9f6549970a835dd574f52906aa1ca05a3ba1
Steffano <smalagadev@gmail.com>
2019-09-23 19:36:16 -0400

    Mongoose added

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+// add paths dependency
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/sweat-it-db', { useNewUrlParser : true });
 
 require("./routes/html-routes.js")(app);
 // include api routes
 
 app.listen(PORT, ()=>{
-  console.log(`We listenin at PORT: ${PORT}`);
+  console.log(`Connected at PORT: ${PORT}`);
 });
